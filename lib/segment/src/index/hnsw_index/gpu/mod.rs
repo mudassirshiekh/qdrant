78a1a5bb09b076df2a6e05e648f7094a73301151
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2024-11-26 11:21:04 +0100

    add timeout comment

--- lib/segment/src/index/hnsw_index/gpu/mod.rs
+++ lib/segment/src/index/hnsw_index/gpu/mod.rs
@@ -30,6 +30,10 @@ lazy_static! {
     pub static ref GPU_DEVICES_MANAGER: OperationResult<DevicesMaganer> = init_devices_manager();
 }
 
+/// Each GPU operation has a timeout by Vulkan API specification.
+/// Choose large enough timeout.
+/// We cannot use too small timeout and check stopper in the loop because
+/// GPU resources should be alive while GPU operation is in progress.
 static GPU_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(60);
 
 static GPU_INDEXING: AtomicBool = AtomicBool::new(false);
